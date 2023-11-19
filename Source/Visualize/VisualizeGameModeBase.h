15761ba234e8f4d57eee55b3014131d3302659e3
Luke Madera <luke.madera@gmail.com>
2023-11-18 16:04:02 -0800

    New empty project

---
+++ Source/Visualize/VisualizeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VisualizeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VISUALIZE_API AVisualizeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
